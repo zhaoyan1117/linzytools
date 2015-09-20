c5df74eb23bdcb911086d4a510d3f56bb234c8ec
Yan Zhao <zhaoyan1117@gmail.com>
2015-09-20 11:46:03 -0700

    wip

---
+++ app/controllers/newsletter_volusion_controller.rb
@@ -0,0 +1,4 @@
+class NewsletterVolusionController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/newsletter/volusion', to: 'newsletter_volusion#index', as: 'newslettervolusion'
 end
