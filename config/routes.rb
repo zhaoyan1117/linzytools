200f8c1d582f91f75e8827e555eebe36b35bc324
Yan Zhao <zhaoyan1117@gmail.com>
2015-10-04 16:23:43 -0700

    use pre

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'newsletter_mailchimp#new_product'
+  root 'welcome#index'
 
   get '/newsletter/volusion/new_product', to: 'newsletter_volusion#new_product', as: 'newsletter_volusion_new_product'
   post '/newsletter/volusion/new_product', to: 'newsletter_volusion#new_product_build', as: 'newsletter_volusion_new_product_build'
